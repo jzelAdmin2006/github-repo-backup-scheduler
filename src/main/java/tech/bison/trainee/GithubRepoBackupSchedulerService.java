f5f5cd87ecdaec8b0e3b63e34a56ee1531bd7251
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-09 12:04:58 +0200

    increase backup frequency

--- src/main/java/tech/bison/trainee/GithubRepoBackupSchedulerService.java
+++ src/main/java/tech/bison/trainee/GithubRepoBackupSchedulerService.java
@@ -21,60 +21,60 @@ import com.google.gson.JsonParser;
 
 @Service
 public class GithubRepoBackupSchedulerService {
-	private static final String GITHUB_TOKEN = System.getenv("GITHUB_TOKEN");
-	private static final String BACKUPPER_TOKEN = System.getenv("BACKUPPER_TOKEN");
-	private static final String ENDPOINT = System.getenv("ENDPOINT");
+  private static final String GITHUB_TOKEN = System.getenv("GITHUB_TOKEN");
+  private static final String BACKUPPER_TOKEN = System.getenv("BACKUPPER_TOKEN");
+  private static final String ENDPOINT = System.getenv("ENDPOINT");
 
-	@Scheduled(fixedRate = 86400000)
-	public void backupGitHub() {
-		try {
-			String repoUrls = findAllRepoUrls().stream().map(url -> url + ".git").collect(Collectors.joining("\n"));
+  @Scheduled(fixedRate = 3600000)
+  public void backupGitHub() {
+    try {
+      String repoUrls = findAllRepoUrls().stream().map(url -> url + ".git").collect(Collectors.joining("\n"));
 
-			URL url = new URL(ENDPOINT);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("token", BACKUPPER_TOKEN);
-			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			conn.setDoOutput(true);
+      URL url = new URL(ENDPOINT);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("POST");
+      conn.setRequestProperty("token", BACKUPPER_TOKEN);
+      conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+      conn.setDoOutput(true);
 
-			try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
-				wr.writeBytes("repoUrls=" + URLEncoder.encode(repoUrls, "UTF-8"));
-				wr.flush();
-			}
+      try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+        wr.writeBytes("repoUrls=" + URLEncoder.encode(repoUrls, "UTF-8"));
+        wr.flush();
+      }
 
-			int responseCode = conn.getResponseCode();
-			System.out.println("Response code: " + responseCode);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      int responseCode = conn.getResponseCode();
+      System.out.println("Response code: " + responseCode);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	private List<String> findAllRepoUrls() throws IOException {
-		int page = 1;
-		List<String> repoUrls = new ArrayList<>();
-		boolean hasMorePages;
-		do {
-			String apiUrl = "https://api.github.com/user/repos?page=" + page;
-			URL url = new URL(apiUrl);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Authorization", "token " + GITHUB_TOKEN);
-			conn.connect();
-			int responseCode = conn.getResponseCode();
-			if (responseCode != 200) {
-				throw new RuntimeException("HttpResponseCode: " + responseCode);
-			}
-			Reader reader = new InputStreamReader(conn.getInputStream());
-			JsonElement jsonElement = JsonParser.parseReader(reader);
-			JsonArray reposArray = jsonElement.getAsJsonArray();
-			hasMorePages = reposArray.size() > 0;
-			for (JsonElement repoElement : reposArray) {
-				JsonObject repoObject = repoElement.getAsJsonObject();
-				String repoUrl = repoObject.get("html_url").getAsString();
-				repoUrls.add(repoUrl);
-			}
-			page++;
-		} while (hasMorePages);
-		return repoUrls;
-	}
+  private List<String> findAllRepoUrls() throws IOException {
+    int page = 1;
+    List<String> repoUrls = new ArrayList<>();
+    boolean hasMorePages;
+    do {
+      String apiUrl = "https://api.github.com/user/repos?page=" + page;
+      URL url = new URL(apiUrl);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      conn.setRequestProperty("Authorization", "token " + GITHUB_TOKEN);
+      conn.connect();
+      int responseCode = conn.getResponseCode();
+      if (responseCode != 200) {
+        throw new RuntimeException("HttpResponseCode: " + responseCode);
+      }
+      Reader reader = new InputStreamReader(conn.getInputStream());
+      JsonElement jsonElement = JsonParser.parseReader(reader);
+      JsonArray reposArray = jsonElement.getAsJsonArray();
+      hasMorePages = reposArray.size() > 0;
+      for (JsonElement repoElement : reposArray) {
+        JsonObject repoObject = repoElement.getAsJsonObject();
+        String repoUrl = repoObject.get("html_url").getAsString();
+        repoUrls.add(repoUrl);
+      }
+      page++;
+    } while (hasMorePages);
+    return repoUrls;
+  }
 }
